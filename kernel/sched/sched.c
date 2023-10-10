0b5bd1161656528c2b062708ee5608c53480ecd5
Glucose-180 <Glucose180@163.com>
2023-10-10 10:23:16 +0800

    Fix sched.c: Don't let pcb0 join ready_queue and scheduler

--- kernel/sched/sched.c
+++ kernel/sched/sched.c
@@ -73,8 +73,9 @@ pid_t alloc_pid(void)
 pid_t create_proc(const char *taskname)
 {
 	ptr_t entry, user_stack, kernel_stack;
-	pcb_t *pnew, *temp;
+	pcb_t *pnew, *temp, *p0;
 
+	p0 = NULL;
 	entry = load_task_img(taskname);
 	if (entry == 0U)
 		return INVALID_PID;
@@ -82,12 +83,24 @@ pid_t create_proc(const char *taskname)
 	kernel_stack = (ptr_t)kmalloc_g(Kstask_size);
 	if (user_stack == 0 || kernel_stack == 0)
 		return INVALID_PID;
+	if (ready_queue->pid == pid0_pcb.pid)
+	{
+		ready_queue = del_node(ready_queue, ready_queue, &p0);
+		if (ready_queue != NULL || p0 == NULL || p0->pid != pid0_pcb.pid)
+			panic_g("create_proc: Failed to remove the proc whose ID is 0");
+	}
 	if ((temp = add_node_to_tail(ready_queue, &pnew)) == NULL)
 	{
 		ufree_g((void *)user_stack);
 		return INVALID_PID;
 	}
 	ready_queue = temp;
+	if (p0 != NULL)
+	{	/* Has removed the 0 proc */
+		p0->next = ready_queue;
+		if (p0 != current_running)
+			panic_g("create_proc: Error happened while removing the proc 0");
+	}
 	init_pcb_stack(kernel_stack + ROUND(Kstask_size, ADDR_ALIGN),
 		user_stack + ROUND(Ustack_size, ADDR_ALIGN), entry, pnew);
 	return pnew->pid;
@@ -116,6 +129,7 @@ void do_scheduler(void)
 	// TODO: [p2-task1] Modify the current_running pointer.
 	// TODO: [p2-task1] switch_to current_running
 	while (1)
+	{
 		for (p = current_running->next; p != current_running; p = p->next)
 		{	/* Search the linked list and find a READY process */
 			if (p->status == TASK_READY)
@@ -128,6 +142,8 @@ void do_scheduler(void)
 				return;
 			}
 		}
+		return;
+	}
 }
 
 void do_sleep(uint32_t sleep_time)
