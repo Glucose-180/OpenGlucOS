0b25189cba539f7470d30041b0ac2cd4c5dc52c4
Glucose-180 <Glucose180@163.com>
2023-10-17 21:41:04 +0800

    Test timer interrupt

--- kernel/irq/irq.c
+++ kernel/irq/irq.c
@@ -40,6 +40,7 @@ void handle_irq_timer(regs_context_t *regs, uint64_t stval, uint64_t scause)
 	// TODO: [p2-task4] clock interrupt handler.
 	// Note: use bios_set_timer to reset the timer and remember to reschedule
 	printk("Timer interrupt comes!\n");	//Only for TEST
+	set_preempt();	/* Reset timer */
 }
 
 void init_exception()
