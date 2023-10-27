0bcf4f52b5aefd31326c9c03870473c865d9194e
Glucose-180 <Glucose180@163.com>
2023-10-27 15:54:25 +0800

    Format: TAB

--- test/test_project3/glush.c
+++ test/test_project3/glush.c
@@ -9,23 +9,23 @@
 
 int main(void)
 {
-    sys_move_cursor(0, SHELL_BEGIN);
-    printf("------------------- Terminal -------------------\n");
-    printf("> root@UCAS_OS: ");
+	sys_move_cursor(0, SHELL_BEGIN);
+	printf("------------------- Terminal -------------------\n");
+	printf("> root@UCAS_OS: ");
 
-    while (1)
-    {
-        // TODO [P3-task1]: call syscall to read UART port
-        
-        // TODO [P3-task1]: parse input
-        // note: backspace maybe 8('\b') or 127(delete)
+	while (1)
+	{
+		// TODO [P3-task1]: call syscall to read UART port
+		
+		// TODO [P3-task1]: parse input
+		// note: backspace maybe 8('\b') or 127(delete)
 
-        // TODO [P3-task1]: ps, exec, kill, clear    
+		// TODO [P3-task1]: ps, exec, kill, clear    
 
-        /************************************************************/
-        /* Do not touch this comment. Reserved for future projects. */
-        /************************************************************/    
-    }
+		/************************************************************/
+		/* Do not touch this comment. Reserved for future projects. */
+		/************************************************************/    
+	}
 
-    return 0;
+	return 0;
 }
